6f42ddefa9303218c39414e5e02bd3e04b0119a4
Asafe Orneles <asafeorneles@gmail.com>
2025-10-14 23:45:58 -0300

    Streams pt 4

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamsTest04.java
@@ -0,0 +1,48 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamsTest04 {
+    public static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 3.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no Life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 6.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+    public static void main(String[] args) {
+        List<List<String>> cruzeiro = new ArrayList<>();
+
+        List<String> atacantes = List.of("Kaio Jorge", "Arroyo", "Gabigol");
+        List<String> volantes = List.of("Lucas Silva", "Lucas Romero", "Christian");
+        List<String> defensores = List.of("Villalba", "Fabrício Bruno", "Cássio");
+
+        cruzeiro.add(atacantes);
+        cruzeiro.add(volantes);
+        cruzeiro.add(defensores);
+
+
+        for (List<String> list : cruzeiro){
+            for (String s : list){
+                System.out.println(s);
+            }
+        }
+
+        System.out.println("-------------");
+
+        cruzeiro.stream() // O cruzeiro.stream() cria um Stream<List<String>>
+                .flatMap(List::stream) // O List::stream fala: Para cada lista (por exemplo, atacantes, volantes, defensores), crie um Stream<String> com os elementos dessa lista. Isso permite que a gente trabalhe com os Elementos da Lista, as Strings.
+                // Entao, temos: Stream<Stream<String>>
+                // O flatMap vê que cada elemento (List<String>) vira um Stream<String> e que está dentro de outro Stream<>, então ele achata, desembrulha esse Stream, retornando apenas Stream<String>
+                .forEach(System.out::println); // Com o Stream<String>, podemos trabalhar diretamente com as Strings, neste caso, printamos.
+    }
+
+}
