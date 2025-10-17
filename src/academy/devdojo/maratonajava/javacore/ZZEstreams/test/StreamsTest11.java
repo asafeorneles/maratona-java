88d028f8a9ada4beabdad304f1f703ff41b7bd8b
Asafe Orneles <asafeorneles@gmail.com>
2025-10-17 15:51:21 -0300

    Streams pt 11

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamsTest11.java
@@ -0,0 +1,50 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Collectors -> Summarizing
+public class StreamsTest11 {
+    public static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 10.99),
+            new LightNovel("Overlord", 3.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no Life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 6.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+
+    public static void main(String[] args) {
+        // USANDO APENAS STREAM x USANDO COLLECTORS
+        // Contar elementos na lista:
+        System.out.println(lightNovels.stream().count());
+        System.out.println(lightNovels.stream().collect(Collectors.counting()));
+
+        // Somar dados/elementos da lista:
+        System.out.println(lightNovels.stream().mapToDouble(LightNovel::getPrice).sum());
+        System.out.println(lightNovels.stream().collect(Collectors.summingDouble(LightNovel::getPrice)));
+
+        // Pegando o maior/menor elemento ou valor da lista:
+        lightNovels.stream().max(Comparator.comparing(LightNovel::getPrice)).ifPresent(System.out::println);
+        lightNovels.stream().collect(Collectors.maxBy(Comparator.comparing(LightNovel::getPrice))).ifPresent(System.out::println);
+
+        // Average -> média dos valores dos elementos da lista
+        lightNovels.stream().mapToDouble(LightNovel::getPrice).average().ifPresent(System.out::println);
+        System.out.println(lightNovels.stream().collect(Collectors.averagingDouble(LightNovel::getPrice)));
+
+        // Summarizing -> Retorna tudo aquilo ali em cima em uma única linha de código.
+        DoubleSummaryStatistics collect = lightNovels.stream().collect(Collectors.summarizingDouble(LightNovel::getPrice));
+        System.out.println(collect);
+
+        // Joining -> joining() coleta os atributos dos elementos da lista que pedimos e ainda os separa do jeito que quisermos (Ele não coloca a separação no ultimo elemento, o joining é um prefeito!)
+        String titles = lightNovels.stream().map(LightNovel::getTitle).collect(Collectors.joining(", "));
+        System.out.println(titles);
+    }
+}
